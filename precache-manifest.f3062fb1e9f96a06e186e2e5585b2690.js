58538476028d2a17543e435bd10b62c9fbef8203
郑茜 <zhenq021@gmail.com>
2020-06-29 20:33:47 +0800

    init

---
+++ precache-manifest.f3062fb1e9f96a06e186e2e5585b2690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18cda7efc8197f53f81ffbee2ec433cb",
+    "url": "/copperdaily-react-website/index.html"
+  },
+  {
+    "revision": "d5e501c4bb2baa511ef1",
+    "url": "/copperdaily-react-website/static/css/main.fbda4940.chunk.css"
+  },
+  {
+    "revision": "80792f12c448bb27d8c5",
+    "url": "/copperdaily-react-website/static/js/2.f0efcd49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/copperdaily-react-website/static/js/2.f0efcd49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5e501c4bb2baa511ef1",
+    "url": "/copperdaily-react-website/static/js/main.1f1285c8.chunk.js"
+  },
+  {
+    "revision": "5ba31f5c3fb260d5892a",
+    "url": "/copperdaily-react-website/static/js/runtime-main.2b95d007.js"
+  }
+]);
